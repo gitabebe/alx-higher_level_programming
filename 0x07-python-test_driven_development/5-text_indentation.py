327a1c77d20abd2dfb48a7aa8e727a442045c1ba
Abebe <142109195+gitabebe@users.noreply.github.com>
2024-01-07 12:43:43 -0800

    Test-driven development

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a text-indentation function."""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    s = text[:]
+
+    for d in ".?:":
+        list_text = s.split(d)
+        s = ""
+        for i in list_text:
+            i = i.strip(" ")
+            s = i + d if s is "" else s + "\n\n" + i + d
+
+    print(s[:-3], end="")
