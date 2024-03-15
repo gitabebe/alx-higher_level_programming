bbc7f2c89d4b4f200ce5194a3016fb48de87ef6d
Abebe <142109195+gitabebe@users.noreply.github.com>
2024-03-15 11:23:44 +0300

    lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """lists all states with a name starting with N"""
-import MySQLdb                                    from sys import argv
+import MySQLdb
+from sys import argv
 
 if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
