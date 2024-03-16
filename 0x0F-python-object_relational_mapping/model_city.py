d4bea563a1a122501571d1250a4e262287543933
Abebe <142109195+gitabebe@users.noreply.github.com>
2024-03-17 00:45:33 +0300

    contains class definition of a City

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class City and an instance Base = declarative_base()"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """links to the MySQL table cities
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Cities.
+        id (sqlalchemy.Integer): The city's id.
+        name (sqlalchemy.String): The city's name.
+        state_id (sqlalchemy.String): The city's state id.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
