4f90b96479968982d7a4c0e5c5564f17ba7d6e7e
Abebe <142109195+gitabebe@users.noreply.github.com>
2024-03-16 18:13:24 +0300

    takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT cities.id, cities.name FROM cities
+    cur.execute("""SELECT cities.name FROM cities
                  JOIN states ON cities.state_id = states.id
                 WHERE states.name LIKE BINARY %(state_name)s
                 ORDER BY cities.id ASC""",
