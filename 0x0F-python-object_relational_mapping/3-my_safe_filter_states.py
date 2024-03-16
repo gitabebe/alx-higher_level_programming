d0ae2a83d64e79c4f425f28f8b17d61ecf68be37
Abebe <142109195+gitabebe@users.noreply.github.com>
2024-03-16 15:13:36 +0300

    takes in arguments and displays all values in the states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values where name mathes"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY %(name)s \
+                 ORDER BY states.id ASC", {'name': argv[4]})
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
