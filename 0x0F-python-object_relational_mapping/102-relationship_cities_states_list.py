8a592ff2ac8fdf8d397bca64ed4da88a242b0e0e
Abebe <142109195+gitabebe@users.noreply.github.com>
2024-03-16 17:20:25 -0700

    lists all City objects from the database hbtn_0e_101_usa

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""lists all City objects from the database hbtn_0e_101_usa"""
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm.session import sessionmaker, Session
+from sqlalchemy import create_engine
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for city in session.query(City).order_by(City.id):
+        print('{}: {} -> {}'.format(city.id, city.name, city.state.name))
