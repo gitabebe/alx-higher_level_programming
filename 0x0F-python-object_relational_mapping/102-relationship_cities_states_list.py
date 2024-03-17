4c7cf6e44cdf5887c8be1c273048bb2ee1c5d4b2
Abebe <142109195+gitabebe@users.noreply.github.com>
2024-03-16 17:23:21 -0700

    lists all City objects from the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -15,5 +15,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for city in session.query(City).order_by(City.id):
-        print('{}: {} -> {}'.format(city.id, city.name, city.state.name))
+    st = session.query(State).join(City).order_by(City.id).all()
+    for state in st:
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
